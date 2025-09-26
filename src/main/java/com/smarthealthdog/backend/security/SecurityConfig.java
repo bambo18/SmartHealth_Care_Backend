9b8de3d374b45f3dd35067a1a81d1e27cbb8552c
Soojong Wi <ud293nbqda@outlook.kr>
2025-09-26 18:50:40 +0900

    Feat: 보안 설정 관련 기능 추가
    - JWT를 사용하도록 설정

---
+++ src/main/java/com/smarthealthdog/backend/security/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.smarthealthdog.backend.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Autowired
+    private AuthEntryPointJWT unauthorizedHandler;
+
+    @Bean
+    public JWTTokenFilter authenticationJwtTokenFilter() {
+        return new JWTTokenFilter();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf((csrf) -> csrf.disable())
+            .exceptionHandling(exceptionHandling ->
+                exceptionHandling.authenticationEntryPoint(unauthorizedHandler)
+            )
+            .sessionManagement(sessionManagement ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                    "/api/auth/**"
+                ).permitAll()
+                .anyRequest().authenticated() 
+            );
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
