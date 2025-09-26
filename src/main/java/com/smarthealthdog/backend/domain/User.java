cbb3dcb5f0e41547a78386d05422b78ce049e7f0
Soojong Wi <ud293nbqda@outlook.kr>
2025-09-26 18:36:03 +0900

    Docs: README.md 수정
    - 메일 설정 방법 수정

--- src/main/java/com/smarthealthdog/backend/domain/User.java
+++ src/main/java/com/smarthealthdog/backend/domain/User.java
@@ -13,7 +13,6 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.sql.Timestamp;
 import java.time.Instant;
 
 @Getter
