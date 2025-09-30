1aa457bf6bdbfd004ac3a8c3f37653d03a225780
Soojong Wi <ud293nbqda@outlook.kr>
2025-09-30 11:45:50 +0900

    Feat: 테스트 전용 설정 사용 로직 추가

--- src/test/java/com/smarthealthdog/backend/BackendApplicationTests.java
+++ src/test/java/com/smarthealthdog/backend/BackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.smarthealthdog.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
 	@Test
