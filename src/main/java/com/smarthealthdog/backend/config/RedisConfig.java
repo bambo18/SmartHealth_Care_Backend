b81639d05d24750134c9235a5bba934165a26f89
Soojong Wi <ud293nbqda@outlook.kr>
2025-11-01 00:20:44 +0900

    Feat: Quartz, Redis 관련 설정 클래스 추가

---
+++ src/main/java/com/smarthealthdog/backend/config/RedisConfig.java
@@ -0,0 +1,17 @@
+package com.smarthealthdog.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
+@Profile("dev")
+public class RedisConfig {
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
+}
