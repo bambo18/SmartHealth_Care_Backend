6b47cffa4df956742a2674ff58485d70f8932c50
Soojong Wi <ud293nbqda@outlook.kr>
2025-11-11 23:07:40 +0900

    Feat: 진단 정보 불러오기 API 엔드포인트 추가

--- src/main/java/com/smarthealthdog/backend/controllers/SubmissionController.java
+++ src/main/java/com/smarthealthdog/backend/controllers/SubmissionController.java
@@ -4,12 +4,17 @@ import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.smarthealthdog.backend.dto.diagnosis.get.SubmissionDetail;
 import com.smarthealthdog.backend.dto.diagnosis.update.SubmissionResultRequest;
 import com.smarthealthdog.backend.services.SubmissionService;
 
@@ -30,5 +35,16 @@ public class SubmissionController {
     ) {
         submissionService.completeDiagnosis(submissionId, request);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('can_view_own_health_records')")
+    public ResponseEntity<SubmissionDetail> getSubmissionById(
+            @PathVariable("id") UUID submissionId,
+            @RequestParam(value = "languageCode", required = false) String languageCode,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        Long userId = Long.parseLong(userDetails.getUsername());
+        return ResponseEntity.ok(submissionService.getSubmissionAndDiagnosesById(submissionId, languageCode, userId));
     }
 }
