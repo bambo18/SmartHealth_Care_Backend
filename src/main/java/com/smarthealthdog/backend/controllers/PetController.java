4dedf6163a24484ba1edbb165c90087c08a5ebd2
Soojong Wi <ud293nbqda@outlook.kr>
2025-11-25 14:02:46 +0900

    Feat: 소변 검사 요청 엔드포인트 추가

--- src/main/java/com/smarthealthdog/backend/controllers/PetController.java
+++ src/main/java/com/smarthealthdog/backend/controllers/PetController.java
@@ -110,7 +110,7 @@ public class PetController {
         return ResponseEntity.ok(PetResponse.from(updatedPet));
     }
 
-    @PostMapping("/{id}/diagnoses/eye")
+    @PostMapping("/{id}/submissions/eye")
     @PreAuthorize("hasAuthority('can_use_health_check')")
     public ResponseEntity<Void> addEyeDiagnosis(
             @PathVariable Long id,
@@ -119,6 +119,18 @@ public class PetController {
     ) {
         Long ownerId = Long.parseLong(userDetails.getUsername());
         aiDiagnosisClientService.performEyeDiagnosis(image, id, ownerId);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/{id}/submissions/urine")
+    @PreAuthorize("hasAuthority('can_use_health_check')")
+    public ResponseEntity<Void> addUrineDiagnosis(
+            @PathVariable Long id,
+            @RequestPart(value = "image") MultipartFile image,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        Long ownerId = Long.parseLong(userDetails.getUsername());
+        aiDiagnosisClientService.performUrineDiagnosis(image, id, ownerId);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
