be3726bdb5fdebac43eab661bf1fc0bd6a52e4ec
Soojong Wi <ud293nbqda@outlook.kr>
2025-11-25 13:56:51 +0900

    Feat: Celery 작업 취소 함수 추가

--- src/main/java/com/smarthealthdog/backend/utils/DiagnosisTaskRequestClient.java
+++ src/main/java/com/smarthealthdog/backend/utils/DiagnosisTaskRequestClient.java
@@ -10,4 +10,10 @@ public interface DiagnosisTaskRequestClient {
      * @param requestDataList 전송할 진단 요청 데이터의 리스트
      */
     void sendDiagnosisTaskInBatch(List<RequestDiagnosisData> requestDataList); 
+
+    /**
+     * AI 진단 작업을 배치로 제거합니다.
+     * @param celeryTaskStrings 제거할 Celery 작업 문자열의 리스트
+     */
+    void removeDiagnosisTasksInBatch(List<String> celeryTaskStrings);
 }
